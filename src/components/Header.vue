9cf7833e6129f6902f586459ed37361f1089f17d
apc <alexcurran.va@gmail.com>
2020-01-09 14:26:17 -0600

    Initial setup

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header">
+    <h1>Ice Cream Creator</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+
+
+</style>
