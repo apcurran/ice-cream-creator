28797550fb590dae097a57f30a2e6e6bd19d3b40
apc <alexcurran.va@gmail.com>
2020-01-10 16:34:21 -0600

    Added delete functionality

--- src/components/Creations.vue
+++ src/components/Creations.vue
@@ -1,7 +1,12 @@
 <template>
   <main class="main">
       <article v-for="(order, index) in allOrders" :key="index" class="created-order">
-        {{ order }}
+        <ul class="created-order__list">
+          <li class="created-order__item">{{ order.container }}</li>
+          <li v-for="(flavor, index) in order.flavors" :key="index" class="created-order__item">{{ flavor }}</li>
+          <li v-for="(topping, index) in order.toppings" :key="index" class="created-order__item">{{ topping }}</li>
+        </ul>
+        <button @click="removeOrder(order)" class="created-order__delete"></button>
       </article>
   </main>
 </template>
@@ -12,12 +17,28 @@ export default {
     props: ["allOrders"],
     data() {
       return {
-      
+        removeOrder(order) {
+          const orderIndex = this.allOrders.indexOf(order);
+          this.allOrders.splice(orderIndex, 1);
+        }
       }
     }
 }
 </script>
 
 <style>
+
+.created-order {
+  margin: 2rem;
+  padding: 2rem;
+  color: #fff;
+  background-color: #222;
+}
+
+.created-order__delete {
+  background: url(../assets/icon-trash.svg) no-repeat center center;
+  padding: 1rem;
+  cursor: pointer;
+}
 
 </style>
